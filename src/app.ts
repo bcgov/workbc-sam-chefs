7ab851a1f130bb3b7a098e22959ed480275598ca
Branko Bajic <eisoftware@mailfence.com>
2023-09-20 16:03:24 -0700

    Remove unnecessary packages, update dc.yaml

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,7 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import helmet from "helmet"
-import cors from "cors"
 import cron from "node-cron"
 import axios from "axios"
 import { getAll } from "./controllers/sam.controller"
 
-const originURL = process.env.ORIGIN_URL || process.env.OPENSHIFT_NODEJS_ORIGIN_URL || "https://localhost:8000"
-
-const corsOptions = {
-    origin: originURL,
-    credentials: true,
-    optionsSuccessStatus: 200
-}
-
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use(helmet())
 console.log(`CRON SERVER START`)
 cron.schedule("* * * * *", async () => {
     console.log("========== BEGIN CRON JOB ==========")
