3418b9ea889ea65ba4966df65bdf3f16d0816f57
Branko Bajic <eisoftware@mailfence.com>
2024-01-16 22:13:24 -0700

    refresh auth token on expiry

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import cron from "node-cron"
 import axios from "axios"
+import createAuthRefreshInterceptor from "axios-auth-refresh"
 import { getAll } from "./controllers/sam.controller"
 
 console.log(`CRON SERVER START`)
+let serviceAccountToken
 cron.schedule("30 1 * * *", async () => {
     console.log("========== BEGIN CRON JOB ==========")
     if (!process.env.GET_TOKEN_URL || !process.env.CHEFS_SERVICE_ACCOUNT_USERNAME || !process.env.CHEFS_SERVICE_ACCOUNT_PASSWORD
@@ -15,74 +17,55 @@ cron.schedule("30 1 * * *", async () => {
     const ignoreList = JSON.parse(process.env.USERS_TO_IGNORE)?.users
     const users = await getAll()
     if (users && users.length > 0){
-        // Obtain a bearer token to be used for CHEFS authentication //
-        const params = new URLSearchParams()
-        params.append("grant_type", "client_credentials")
-        await axios
-            .post(
-                process.env.GET_TOKEN_URL, 
-                params,
-                {
-                    auth: {
-                        username: `${process.env.CHEFS_SERVICE_ACCOUNT_USERNAME}`,
-                        password: `${process.env.CHEFS_SERVICE_ACCOUNT_PASSWORD}`
-                    },
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    }
+        console.log("num users: ", users.length)
+        const accessToken = await getServiceAccountToken()
+        if (accessToken){
+            serviceAccountToken = accessToken
+            createAuthRefreshInterceptor(axios, refreshAuthLogic, { statusCodes: [401, 403] }) // add token refresh interceptor incase access token expires
+            for (const user of users) {
+                console.log("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+                if (ignoreList.includes(user.username.toLowerCase())) { 
+                    console.log(`ignoring user ${user.username}`)
+                    continue
                 }
-            )
-            .then(async (response) => {
-                const accessToken = response?.data?.access_token
-                if (accessToken){
-                    for (const user of users) {
-                        console.log("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-                        if (ignoreList.includes(user.username.toLowerCase())) { 
-                            console.log(`ignoring user ${user.username}`)
-                            continue
-                        }
-                        // For each user, retrieve their unique CHEFS user ID, then give them access to the needed forms //
-                        const userResponse = await axios.get(
-                            `${process.env.CHEFS_API_URL}/users/`,
-                            {
-                                params: {
-                                    idpUserId: user.guid
-                                },
-                                headers: {
-                                    "Authorization": `Bearer ${accessToken}`
-                                }
-                            }
-                        )
-                        .catch((error) => {
-                            console.log(error.message)
-                            throw new Error
-                        })
-                        let chefsUserID
-                        if (userResponse?.data?.length > 1){
-                            console.log(`user ${user.username} returned multiple results in CHEFS - skipping`)
-                            continue
-                        }
-                        if (userResponse?.data?.length === 0){
-                            console.log(`user ${user.username} not found in CHEFS - creating user...`)
-                            chefsUserID = await createUser(accessToken, user.guid, user.username)
-                        }
-                        else if (userResponse?.data?.length === 1){ // only give user form permissions if one match was found
-                            chefsUserID = userResponse.data[0].id
-                        }
-                        if (chefsUserID){
-                            console.log(`Beginning permission granting for user ${user.username} with chefs userID ${chefsUserID}...`)
-                            await giveFormPermissions(process.env.NEED_EMPLOYEE_FORM_ID as string, chefsUserID, user, accessToken, false)
-                            await giveFormPermissions(process.env.HAVE_EMPLOYEE_FORM_ID as string, chefsUserID, user, accessToken, false)
-                            await giveFormPermissions(process.env.SERVICE_PROVIDER_CLAIM_FORM_ID as string, chefsUserID, user, accessToken, true)
+                // For each user, retrieve their unique CHEFS user ID, then give them access to the needed forms //
+                const userResponse = await axios.get(
+                    `${process.env.CHEFS_API_URL}/users/`,
+                    {
+                        params: {
+                            idpUserId: user.guid
+                        },
+                        headers: {
+                            "Authorization": `Bearer ${serviceAccountToken}`
                         }
                     }
+                )
+                .catch((error) => {
+                    console.log(error.message)
+                    throw new Error
+                })
+                let chefsUserID
+                if (userResponse?.data?.length > 1){
+                    console.log(`user ${user.username} returned multiple results in CHEFS - skipping`)
+                    continue
+                }
+                if (userResponse?.data?.length === 0){
+                    console.log(`user ${user.username} not found in CHEFS - creating user...`)
+                    chefsUserID = await createUser(serviceAccountToken, user.guid, user.username)
+                }
+                else if (userResponse?.data?.length === 1){ // only give user form permissions if one match was found
+                    chefsUserID = userResponse.data[0].id
+                }
+                if (chefsUserID){
+                    console.log(`Beginning permission granting for user ${user.username} with chefs userID ${chefsUserID}...`)
+                    await giveFormPermissions(process.env.NEED_EMPLOYEE_FORM_ID as string, chefsUserID, user, serviceAccountToken, false)
+                    await giveFormPermissions(process.env.HAVE_EMPLOYEE_FORM_ID as string, chefsUserID, user, serviceAccountToken, false)
+                    await giveFormPermissions(process.env.SERVICE_PROVIDER_CLAIM_FORM_ID as string, chefsUserID, user, serviceAccountToken, true)
                 }
             }
-            )
-            .catch((error) => {
-                console.log(error)
-                return error
-            })
+        } else {
+            console.log("no access token found in response")
+        }
     }
     console.log("========== END CRON JOB ==========")
 })
@@ -151,3 +134,36 @@ export const createUser = async (token: string, userGUID: string, username: stri
         throw new Error()
     }
 }
+
+export const getServiceAccountToken = async () => {
+    // Obtain a bearer token to be used for CHEFS authentication //
+    const params = new URLSearchParams()
+    params.append("grant_type", "client_credentials")
+    const res = await axios
+        .post(
+            process.env.GET_TOKEN_URL as string, 
+            params,
+            {
+                auth: {
+                    username: `${process.env.CHEFS_SERVICE_ACCOUNT_USERNAME}`,
+                    password: `${process.env.CHEFS_SERVICE_ACCOUNT_PASSWORD}`
+                },
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            }
+        )
+        .catch((error) => {
+            console.log(error)
+            throw new Error("service account token request failed")
+        })
+    return res?.data?.access_token
+}
+
+export const refreshAuthLogic = (failedRequest: any) =>
+    getServiceAccountToken().then((token: string) => {
+        console.log("a request failed; received auth token; updating header")
+        failedRequest.response.config.headers['Authorization'] = 'Bearer ' + token
+        serviceAccountToken = token
+        return Promise.resolve()
+    })
